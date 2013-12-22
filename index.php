872ff2e2281b69c21591a4243d157356e45f1c1b
Ananth Madhavan <ananthmadhavan6@gmail.com>
2013-12-23 03:25:53 +0530

    Styling the results and input field.

--- index.php
+++ index.php
@@ -7,7 +7,8 @@ $CONSUMER_SECRET = "MOPAHA3fpFqLpFxMc2K7hIr9pWMaihyo3RSct5N1I";
 $ACCESS_TOKEN = "333910829-OfVKP3PWz8gYzPKXJNKxM4DmYnzlzV6DW8Yoytqe";
 $ACCESS_SECRET = "GWWSzAV79wUTN6waDOD1MbghpRqhRmaeRTQYuwEFhsu3a";
 $connection = new TwitterOAuth($CONSUMER_KEY,$CONSUMER_SECRET,$ACCESS_TOKEN,$ACCESS_SECRET);
-
+//var_dump($connection);
+//int error_reporting [E_ALL]);
 ?>
 
 <html>
@@ -21,22 +22,51 @@ $connection = new TwitterOAuth($CONSUMER_KEY,$CONSUMER_SECRET,$ACCESS_TOKEN,$ACC
 <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
 
 <body>
-	<h1>Tweets check </h1>
-	<form action=" " method= 'POST'>
-		<div class="col-lg-3">
-			Enter search query: <input type="text" class="form-control" name="search">
-		</div>
-	</form>
-	<?php
-		//var_dump($_POST);die; 
-		if(isset($_POST['search'])){
-			$tweets = $connection->get('https://api.twitter.com/1.1/search/tweets.json?q='.$_POST['search'].'&result_type=recent&count=50');
-			foreach ($tweets as $tweet) {
-				foreach ($tweet as $t) {
-					echo $t->text.'<br>';
-				}
-			}
-	} ?>
+<div class = "container">
+	<div class = "row"> 
+		<div class="col-lg-6" style="float:none !important; margin:0 auto;">
+		<h1 style="text-align:center;">Tweet Search </h1>
+
+		<form action=" " method= 'POST' class = 'bs-example form-horizontal'>
+			<div class="form-group">
+				<label for="query" class="col-lg-3 control-label">Enter Hashtag</label>
+				<div class="col-lg-9">
+					<input type="text" class="form-control" name="search">
+				</div>
+				 
+			</div>
+		</form>
+		</div> 
+	</div> <!-- End input row -->
+</div> <!-- End of container. -->
+<div class="container">
+	<div class="row">
+		<div class="col-lg-6" style="float:none !important; margin:0 auto;">
+			<div class="well">
+				<fieldset>
+					<legend>Results</legend>
+					
+					<?php
+					//var_dump($_POST);die; 
+					if(isset($_POST['search'])){
+						//need to encode the search query to match the URL.
+						$hashTag = urlencode($_POST['search']);
+						$query = "https://api.twitter.com/1.1/search/tweets.json?q=".$hashTag."&result_type=recent&count=50";
+						$tweets = $connection->get($query);
+						foreach ($tweets as $tweet) {
+							foreach ($tweet as $t) {
+								
+								//<!--<div id="paginate"> -->
+								echo $t->text;
+								echo "<hr>";
+							}
+						}
+					} ?>
+				</fieldset>
+			</div> <!-- End well -->
+		</div> <!-- End col -->
+	</div><!-- End row -->
+</div> <!-- End container -->
 </body>
 </html>
 
